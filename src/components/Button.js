cd4a84826f79d9af12069d785b83c04ad0bd9378
Samsoucy <newf930@outlook.com>
2020-05-27 11:39:25 -0400

    added styled components to navbar

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+text-transform:capitalize;
+font-size: 1.4rem;
+background: var(--secondaryRed);
+border: 0.2rem solid var(--mainRed);
+color: var(--mainBlack);
+border-radius: 0.5rem;
+padding: 0.2 rem 0.5rem;
+cursor:pointer;
+margin: 0.2 0.5 0.2 0;
+transition: all 0.5s ease-in-out;
+`
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../mtgLogo.png"
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component{
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to="/">
                     <img src={logo} alt="store"
                         className="navbar-brand" />
@@ -18,13 +20,24 @@ export default class Navbar extends Component{
                 </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <button>
-                        <i className="fas fa-cart-plus" />
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus" />
+                        </span>
                         cart
-                    </button>
+                    </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
             
         );
     }
 }
+
+const NavWrapper = styled.nav`
+background: var(--mainBlack);
+.nav-link{
+    color: var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+}
+`
