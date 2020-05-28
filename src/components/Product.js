a3ac8c8b5e2b799a650530de67938a4d1cb911d6
Samsoucy <newf930@outlook.com>
2020-05-27 21:20:10 -0400

    added display info for products and added style components to products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,99 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component{
     render() {
+        const { id, title, img, price, inCart } = this.props.product;
         return (
-            <div>
-                <h3>
-                    hello from product
-                </h3>
-            </div>
+            <ProductWrapper className= "col-9 max-auto col-md-6 col-lg-6 my-6">
+                <div className="card">
+                    <div
+                        className="img-container p-5"
+                        onClick={() =>console.log("you clickedon me in the image container")}>
+                    
+                    <Link to="/details">
+                        <img src={img} alt="product" className="card-img-top"/>
+                    </Link>
+                        <button className="cart-btn" disabled={inCart ? true : false} onClick={() => {
+                            console.log("added to the cart");
+                        }}
+                        >
+                           {inCart ? (
+                            <p classNamw="text-capitalize mb-0" disabled>
+                                {" "}
+                                in Cart
+                            </p>
+                        ) : (
+                        <i className="fas fa-cart-plus"/>
+                        )} 
+                        </button>
+                    </div> 
+                    {/* card footer*/}
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">
+                       {title}
+                        </p>
+                        <h5 className="text-black font-italic mb-0">
+                            <span className="mr-1">$</span>
+                            {price}
+                        </h5>
+                    </div>
+                </div>
+            </ProductWrapper>
         );
     }
 }
+
+const ProductWrapper = styled.div`
+.card{
+    border-color: transparent;
+    transition: all 1s linear;
+}
+.card-footer{
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+}
+&:hover{
+    .card{
+        border:0.04rem solid var(--secondaryBlack);
+        box-shadow:2px 2px 5px 0px var(--secondaryBlack);
+    }
+    .card-footer{
+        background: rgba(247, 247, 247);
+    }
+}
+.img-container{
+    position: relative;
+    overflow: hidden;
+}
+.card-img-top{
+    transition: all 1s linear;
+}
+.img-container:hover .card-img-top{
+    transform: scale(1.2);
+}
+.cart-btn{
+    position: absolute;
+    bottom:0;
+    right:0;
+    padding: 0.2rem 0.4rem;
+    background: var(--secondaryRed);
+    border: none;
+    color: var(--mainBlack);
+    font-size:1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 1s linear;
+}
+.img-container:hover .cart-btn{
+    transform: translate(0, 0);
+    
+}
+.cart-btn:hover{
+    color: var(--mainGreen);
+    cursor: pointer;
+}
+`;
--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const magicProducts = [
     {
         id: 1,
         title: "Lorem ipsum dolor amet",
-        img: "Lorem ipsum dolor amet",
+        img: "img/magiccard-2.png",
         price: 0,
         info: "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
         inCart: false,
