8312299fa5b3671aae384dac583101da7208bd32
Samsoucy <newf930@outlook.com>
2020-06-01 21:39:44 -0400

    set up cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Title from "../Title";
+import CartColumms from "./CartColumns";
+
+
+export default class Cart extends Component{
+    render() {
+        return (
+            <section>
+                <Title name="Your" title="Cart" />
+                <CartColumns />
+            </section>
+        );
+    }
+}
